81b7ea26516e2918ae4d697c317e180aab5d3b4d
luisiacc <lccpacc@gmail.com>
2021-12-26 15:23:34 -0500

    feat: add configuration for background and some styles

---
+++ lua/gruvbox-ts/colors.lua
@@ -0,0 +1,51 @@
+local palette = {
+  dark = "#202020",
+  foreground = "#ebdbb2",
+  background = "#282828",
+  background_dark = "#242424",
+  bg_light = "#32302f",
+  medium_gray = "#504945",
+  comment = "#665c54",
+  gray = "#DEDEDE",
+  soft_yellow = "#EEBD35",
+  soft_green = "#98971a",
+  bright_yellow = "#fabd2f",
+  orange = "#d65d0e",
+  red = "#fb4934",
+  error_red = "#cc241d",
+  magenta = "#b16286",
+  pink = "#D4879C",
+  light_blue = "#7fa2ac",
+  dark_gray = "#83a598",
+  blue_gray = "#458588",
+  forest_green = "#689d6a",
+  clean_green = "#8ec07c",
+  milk = "#E7D7AD",
+  none = "NONE",
+}
+
+local M = {}
+
+function M.config(config)
+  config = config or require("gruvbox-ts.config")
+  local background = config.background_color or palette.background
+  local intensity_map = {
+    ["dark"] = {
+      background = "#202020",
+      background_dark = "#161616",
+    },
+    ["medium"] = {
+      background = palette.background,
+      background_dark = palette.background_dark,
+    },
+  }
+
+  local colors = palette
+  if intensity_map[background] then
+    colors = vim.tbl_extend("force", colors, intensity_map[background])
+  end
+
+  return colors
+end
+
+return M
---
+++ lua/gruvbox-ts/config.lua
@@ -0,0 +1,25 @@
+local config
+
+-- shim vim for kitty and other generators
+vim = vim or { g = {}, o = {} }
+
+local function opt(key, default)
+  key = "gruvbox_ts_" .. key
+  if vim.g[key] == nil then
+    return default
+  end
+  if vim.g[key] == 0 then
+    return false
+  end
+  return vim.g[key]
+end
+
+config = {
+  background_color = opt("background_color", "medium"),
+  comment_style = opt("comment_style", "italic"),
+  keyword_style = opt("keyword_style", "italic"),
+  function_style = opt("function_style", "bold"),
+  variable_style = opt("variable_style", "NONE"),
+}
+
+return config
