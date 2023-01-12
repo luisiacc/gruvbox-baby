2d9d214baadb5e471d5603b6eba1f4693e114f15
luisiacc <luiscorderoacc@gmail.com>
2023-01-12 02:09:47 -0600

    fix: function highlights

--- after/queries/python/highlights.scm
+++ after/queries/python/highlights.scm
@@ -1,2 +1,2 @@
-(decorator ((call (identifier) @decorator)))
-(decorator ((identifier) @decorator))
+(decorated_definition (decorator ((call (identifier) @decorator))))
+(decorated_definition (decorator (identifier @decorator)))
